995b5779d5fb7ad8388d7a2b6775dd412d6051c3
unknown <tamiloreolaniyi@gmail.com>
2022-03-23 19:52:35 +0100

    poster component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './component/Nav';
+import Poster from './component/Poster';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Poster/>
     </div>
   );
 }
---
+++ src/component/Poster.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Poster.css'
+
+function Poster() {
+  return (
+    <div className='banner'>
+        <img src="./images/poster.png" alt="" />
+    </div>
+  )
+}
+
+export default Poster
