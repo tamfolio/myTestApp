c35b9385143518188276ecc9de1d659fbf2680e3
unknown <tamiloreolaniyi@gmail.com>
2022-03-23 19:44:51 +0100

    navbar

---
+++ src/component/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className='navbar'>
+        <img src="./images/logo.png" alt="" />
+    </div>
+  )
+}
+
+export default Nav
