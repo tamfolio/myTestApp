dbd657dce15ef4548dd51b8c4125af6d7d65663b
unknown <tamiloreolaniyi@gmail.com>
2022-03-24 09:49:33 +0100

    searchbar

--- src/component/Row.jsx
+++ src/component/Row.jsx
@@ -6,9 +6,9 @@ function Row({title, movies}) {
     <div className='row'>
         <h1>{title}</h1>
         <div className="row-items">
-         {movies && Array.isArray(movies) ? movies.map(movie => (
+         {movies && Array.isArray(movies) && movies.length ? movies.map(movie => (
            <img className='row-item' src={movie.Poster} alt="" key={movie.imdbID} />  
-        )): 'No Search Result'}
+        )): movies && movies.Poster ? <img src={movies.Poster} alt=''/>: <p>No Result Found</p>}
         </div>
     </div>
   )
